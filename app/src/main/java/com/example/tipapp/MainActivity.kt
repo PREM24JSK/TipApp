d39bc8a1b63607ba4ef1a01179e0f3d1381cf586
parimal <ashishparmar4@live.com>
2023-06-15 22:23:07 +0530

    Complete Running Tip Calculator Android Training Sample

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -7,40 +7,38 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateBtn.setOnClickListener{calculateTip()}
+        binding.calculateBtn.setOnClickListener {
+            calculateTip()
+        }
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost==null) {
+        val stringText = binding.costOfService.text.toString()
+        val cost = stringText.toDoubleOrNull()
+        if (cost == null) {
             displayTip(0.0)
             return
-
         }
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.tip20 -> 0.20
             R.id.tip18 -> 0.18
-            else ->
+            else -> {
                 0.15
+            }
         }
-        var tip = tipPercentage * cost
-
-        if (binding.switchRound.isChecked) {
-          tip=  ceil(tip)
-        }
-        displayTip(tip)
+        var tipValue= cost*tipPercentage
+        tipValue= ceil(tipValue)
+        displayTip(tipValue)
     }
 
     private fun displayTip(tip: Double) {
         val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipAmount.text=getString(R.string.tip_amount,formattedTip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
+
 }
