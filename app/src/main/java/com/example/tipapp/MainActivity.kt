8a6514b30d9beef981464089c70bf1b986cac8f8
parimal <ashishparmar4@live.com>
2023-06-18 17:46:04 +0530

    Complete Running Tip Calculator Android Training Sample

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.tipapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tipapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -12,13 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.costOfServiceEditText.setOnKeyListener { v, keyCode, event ->
+            handleKeyEvent(v,keyCode)
+        }
         binding.calculateBtn.setOnClickListener {
             calculateTip()
         }
     }
+    private fun handleKeyEvent(view: View, key:Int): Boolean {
+        return if (key== KEYCODE_ENTER) {
+            val inputManager=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(view.windowToken,0)
+            true
+        } else
+            false
 
+    }
     private fun calculateTip() {
-        val stringText = binding.costOfService.text.toString()
+        val stringText = binding.costOfServiceEditText.text.toString()
         val cost = stringText.toDoubleOrNull()
         if (cost == null) {
             displayTip(0.0)
@@ -27,17 +43,24 @@ class MainActivity : AppCompatActivity() {
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.tip20 -> 0.20
             R.id.tip18 -> 0.18
+            R.id.tip18 -> 0.18
             else -> {
-                0.15
+                0.0
             }
         }
-        var tipValue= cost*tipPercentage
-        tipValue= ceil(tipValue)
-        displayTip(tipValue)
+        if (tipPercentage > 0.0) {
+            var tipValue = cost * tipPercentage
+            if (binding.switchRound.isChecked) {
+                tipValue = ceil(tipValue)
+            }
+            displayTip(tipValue)
+        } else {
+            Toast.makeText(this, "Select Rating", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun displayTip(tip: Double) {
-        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
 
